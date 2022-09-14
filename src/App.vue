869207e710659c209dcf40a6d00e3a30c9351517
zhousg <zhoushugang@itcast.cn>
2022-09-14 17:24:17 +0800

    vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <script setup lang="ts">
-import { useUserStore } from './stores'
-
-const store = useUserStore()
+import { Button as VanButton } from 'vant'
 </script>
 
 <template>
-  <div>App组件</div>
-  <div>用户：{{ store.user }}</div>
-  <button @click="store.setUser({ id: '1', mobile: '1', avatar: '1', account: '1', token: '1' })">
-    登录
-  </button>
-  <button @click="store.delUser()">退出</button>
+  <van-button type="success">按钮</van-button>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import pinia from './stores'
+
+// 导入vant的全局样式，后面的main可以覆盖它
+import 'vant/lib/index.css'
 import './styles/main.scss'
 
 const app = createApp(App)
