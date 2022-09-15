c5d18c10ddee032163c56268ce60a072e3eb3d48
zhousg <zhoushugang@itcast.cn>
2022-09-15 17:53:29 +0800

    页面布局-表单

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const agree = ref(false)  
+</script>
 
 <template>
   <div class="login-page">
@@ -11,6 +15,24 @@
       </a>
     </div>
     <!-- form 表单 -->
+    <van-form autocomplete="off">
+      <van-field type="text" placeholder="请输入手机号"></van-field>
+      <van-field type="password" placeholder="请输入密码"></van-field>
+      <div class="cp-cell">
+        <van-checkbox v-model="agree">
+          <span>我已同意</span>
+          <a href="javascript:;">用户协议</a>
+          <span>及</span>
+          <a href="javascript:;">隐私条款</a>
+        </van-checkbox>
+      </div>
+      <div class="cp-cell">
+        <van-button block round type="primary">登 录</van-button>
+      </div>
+      <div class="cp-cell">
+        <a href="javascript:;">忘记密码？</a>
+      </div>
+    </van-form>
     <div class="login-other">
       <van-divider>第三方登录</van-divider>
       <div class="icon">
@@ -49,6 +71,23 @@
         width: 36px;
         height: 36px;
         padding: 4px;
+      }
+    }
+  }
+}
+.van-form {
+  padding: 0 14px;
+  .cp-cell {
+    height: 52px;
+    line-height: 24px;
+    padding: 14px 16px;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    .van-checkbox {
+      a {
+        color: var(--cp-primary);
+        padding: 0 5px;
       }
     }
   }
