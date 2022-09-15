7159fe83ee00dfa1bcde0825c2aaa9fd04f9fad9
zhousg <zhoushugang@itcast.cn>
2022-09-15 15:11:16 +0800

    自动按需加载

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,11 +1,10 @@
-<script setup lang="ts">
-import { Button as VanButton, Checkbox as VanCheckbox } from 'vant'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="login-page">
     <van-button type="primary">按钮</van-button>
     <van-checkbox>复选框</van-checkbox>
+    <van-icon name="chat-o"></van-icon>
   </div>
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,22 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // 默认是/基准路径
   base: '/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      // 默认是 true 开启自动生成组件的类型声明文件，vant的组件已经有类型声明文件，只要导入了就会使用类型声明。
+      dts: false,
+      // 在main.ts已经引入了所有的样式，不需要自动导入要是，只需要自动导入组件即可。
+      resolvers: [VantResolver({ importStyle: false })]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
