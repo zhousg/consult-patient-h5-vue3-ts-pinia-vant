ed2d558dffafe86d2e84afaede50fb397646e1b0
zhousg <zhoushugang@itcast.cn>
2022-09-17 16:39:27 +0800

    个人中心-用户信息类型

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -8,3 +8,18 @@ export type User = {
 
 // 验证码类型
 export type CodeType = 'login' | 'register' | 'changeMobile' | 'forgetPassword' | 'bindMobile'
+
+// 排出token得到对象类型
+type OmitUser = Omit<User, 'token'>
+type UserInfo = OmitUser & {
+  likeNumber: number
+  collectionNumber: number
+  score: number
+  couponNumber: number
+  orderInfo: {
+    paidNumber: number
+    receivedNumber: number
+    shippedNumber: number
+    finishedNumber: number
+  }
+}
