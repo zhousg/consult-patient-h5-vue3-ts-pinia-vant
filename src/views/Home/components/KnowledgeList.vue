80ce49aff9bd2b653c4553c7936dd8ac35be3138
zhousg <zhoushugang@itcast.cn>
2022-09-18 17:05:26 +0800

    首页模块-知识加载-效果

--- src/views/Home/components/KnowledgeList.vue
+++ src/views/Home/components/KnowledgeList.vue
@@ -1,10 +1,35 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import KnowledgeCard from './KnowledgeCard.vue'
+
+// 实现加载更多效果
+const loading = ref(false)
+const finished = ref(false)
+const list = ref<number[]>([])
+const onLoad = () => {
+  // 模拟下加载更多
+  setTimeout(() => {
+    const data = [1, 2, 3, 4, 5]
+    list.value.push(...data)
+    loading.value = false
+    // 模拟数据加载完毕
+    if (list.value.length >= 20) {
+      finished.value = true
+    }
+  }, 1000)
+}
 </script>
 
 <template>
   <div class="knowledge-list">
-    <knowledge-card v-for="i in 5" :key="i"></knowledge-card>
+    <van-list
+      v-model:loading="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <knowledge-card v-for="(item, i) in list" :key="i"></knowledge-card>
+    </van-list>
   </div>
 </template>
 
