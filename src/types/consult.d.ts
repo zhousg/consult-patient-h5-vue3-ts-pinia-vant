d0995da639f14edcfccda1463dfd987e18c413fd
zhousg <zhoushugang@itcast.cn>
2022-09-18 17:23:31 +0800

    首页模块-知识数据-类型

--- src/types/consult.d.ts
+++ src/types/consult.d.ts
@@ -1,2 +1,37 @@
 // 文章类型
 export type KnowledgeType = 'like' | 'recommend' | 'fatReduction' | 'food'
+
+// 文章信息
+export type Knowledge = {
+  id: string
+  title: string
+  coverUrl: string[]
+  topics: string[]
+  collectionNumber: number
+  commentNumber: number
+  creatorName: string
+  creatorAvatar: string
+  creatorHospatalName: string
+  likeFlag: 0 | 1
+  content: string
+  creatorDep: string
+  creatorTitles: string
+  creatorId: string
+}
+
+// 文章列表
+export type KnowledgeList = Knowledge[]
+
+// 分页文章
+export type KnowledgePage = {
+  pageTotal: number
+  total: number
+  rows: KnowledgeList
+}
+
+// 文章查询参数
+export type KnowledgeParams = {
+  type: KnowledgeType
+  current: number
+  pageSize: number
+}
