0a2eb75f8bd0c68e1dc90b7058c76553b20a7563
zhousg <zhoushugang@itcast.cn>
2022-09-23 11:27:10 +0800

    问诊室-默认消息-处理数据

--- src/views/Room/index.vue
+++ src/views/Room/index.vue
@@ -4,10 +4,12 @@ import RoomAction from './components/RoomAction.vue'
 import RoomMessage from './components/RoomMessage.vue'
 import { io } from 'socket.io-client'
 import type { Socket } from 'socket.io-client'
-import { onMounted, onUnmounted } from 'vue'
+import { onMounted, onUnmounted, ref } from 'vue'
 import { baseURL } from '@/utils/rquest'
 import { useUserStore } from '@/stores'
 import { useRoute } from 'vue-router'
+import type { TimeMessages, Message } from '@/types/room'
+import { MsgType } from '@/enums'
 
 const store = useUserStore()
 const route = useRoute()
@@ -16,6 +18,7 @@ const route = useRoute()
 // 组件挂载完毕  建立连接，组件卸载，关闭连接
 
 let socket: Socket
+const list = ref<Message[]>([])
 onUnmounted(() => {
   socket.close()
 })
@@ -42,6 +45,29 @@ onMounted(() => {
   // 发生错误
   socket.on('error', (err) => {
     console.log('发生错误', err)
+  })
+
+  // 聊天记录（默认消息）
+  // 1. 绑定消息记录事件，后台连接成功主动发消息
+  // 2. 拿到消息分组之后，处理成可以的消息列表 Message[]
+  // 3. 响应式数据，接收数据，依据数据渲染即可
+  socket.on('chatMsgList', ({ data }: { data: TimeMessages[] }) => {
+    // 处理消息：分组的时间自己组织成一条通用消息，items取出来放后面
+    const arr: Message[] = []
+    data.forEach((item) => {
+      arr.push({
+        id: item.createTime,
+        msgType: MsgType.Notify,
+        createTime: item.createTime,
+        msg: {
+          content: item.createTime
+        }
+      })
+      arr.push(...item.items)
+    })
+    // 将处理好的数据放置list中
+    list.value.unshift(...arr)
+    console.log(list.value)
   })
 })
 </script>
