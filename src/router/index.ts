183386adf4419fd3207d0d326603246374cb10ef
zhousg <zhoushugang@itcast.cn>
2022-11-30 10:12:49 +0800

    feat: 目录调整和路由代码分析

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+// 如何得到路由实例 createRouter()
+// 如何设置路由模式 history
+// history 模式 createWebHistory()
+// hash 模式 createWebHashHistory()
 const router = createRouter({
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  // 默认参数 '/' 路由的基础路由
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: []
 })
+
+console.log(import.meta)
 
 export default router
