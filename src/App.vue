05d68e332001964cbaa4b3039c54770199d6e0ea
zhousg <zhoushugang@itcast.cn>
2022-12-01 10:52:30 +0800

    feat: 主题定制

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,17 @@ import { Button as VanButton } from 'vant'
 
 <template>
   <div>App <van-button type="primary">按钮</van-button></div>
+  <div class="main">main</div>
+  <div class="footer">footer</div>
 </template>
+
+<style scoped lang="scss">
+.main {
+  color: var(--cp-primary);
+  background-color: var(--footer-color);
+}
+.footer {
+  color: var(--main-color);
+  background-color: var(--footer-color);
+}
+</style>
