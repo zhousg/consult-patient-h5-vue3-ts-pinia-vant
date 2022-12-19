a5268ece455e38a5f24dce4f325424240ee3ebd8
zhousg <zhoushugang@itcast.cn>
2022-12-19 16:50:54 +0800

    feat: 组件的自动注册

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,7 +1,8 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div class="login-page">login</div>
+  <div class="login-page">
+    <van-button>按钮</van-button>
+    <van-checkbox>复选框</van-checkbox>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// 配置组件自动注册的插件
+// 配置 vant UI 组件库的解析器
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // 样式重复引入，类型声明文件重复了
+    Components({
+      dts: false,
+      resolvers: [VantResolver({ importStyle: false })]
+    })
+  ],
   //  base: '/hm',
   resolve: {
     alias: {
