320fb20526d4791acad8df559b0cdeb2b2fce4e2
zhousg <zhoushugang@itcast.cn>
2022-12-20 13:41:00 +0800

    feat: cp-nav-bar组件的结构和样式

---
+++ src/components/CpNavBar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <!-- 固定定位，左侧箭头，标题，右侧文字 -->
+  <van-nav-bar fixed left-arrow title="登录" right-text="注册"></van-nav-bar>
+</template>
+
+<style lang="scss" scoped>
+:deep() {
+  .van-nav-bar {
+    &__arrow {
+      color: var(--cp-text1);
+      font-size: 18px;
+    }
+    &__title {
+      font-size: 15px;
+    }
+  }
+}
+</style>
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="login-page">
+    <cp-nav-bar></cp-nav-bar>
     <van-button>按钮</van-button>
     <van-checkbox>复选框</van-checkbox>
   </div>
