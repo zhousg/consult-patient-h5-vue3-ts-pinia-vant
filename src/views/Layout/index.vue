355ba6507ff5b7208cb8d151c69413a69803de93
zhousg <zhoushugang@itcast.cn>
2022-12-27 16:20:41 +0800

    feat: 布局容器-tabbar切换效果

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -4,8 +4,51 @@
   <div class="layout-page">
     <!-- 二级路由出口 -->
     <router-view></router-view>
-    <div>tabbar</div>
+    <van-tabbar route>
+      <van-tabbar-item to="/home">
+        首页
+        <template #icon="{ active }">
+          <cp-icon :name="`home-index-${active ? 'active' : 'default'}`">
+          </cp-icon>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/article">
+        健康百科
+        <template #icon="{ active }">
+          <cp-icon :name="`home-article-${active ? 'active' : 'default'}`">
+          </cp-icon>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/notify">
+        消息通知
+        <template #icon="{ active }">
+          <cp-icon :name="`home-notice-${active ? 'active' : 'default'}`">
+          </cp-icon>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/user">
+        我的
+        <template #icon="{ active }">
+          <cp-icon :name="`home-mine-${active ? 'active' : 'default'}`">
+          </cp-icon>
+        </template>
+      </van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.layout-page {
+  :deep() {
+    .van-tabbar-item {
+      &__icon {
+        font-size: 21px;
+      }
+      &__text {
+        font-size: 11px;
+        color: var(--cp-text3);
+      }
+    }
+  }
+}
+</style>
