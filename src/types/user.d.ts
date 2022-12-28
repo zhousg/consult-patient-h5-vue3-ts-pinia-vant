f3161c7436158a545aed182f10529888c7b10669
zhousg <zhoushugang@itcast.cn>
2022-12-28 16:13:48 +0800

    类型

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,13 +1,13 @@
 export type User = {
-  /* token令牌 */
+  /** token令牌 */
   token: string
-  /* 用户ID */
+  /** 用户ID */
   id: string
-  /* 用户名称 */
+  /** 用户名称 */
   account: string
-  /* 手机号 */
+  /** 手机号 */
   mobile: string
-  /* 头像 */
+  /** 头像 */
   avatar: string
 }
 
