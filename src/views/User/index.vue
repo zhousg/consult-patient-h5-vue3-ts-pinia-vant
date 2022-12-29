3487d6669243aeecd3f30d0c7f00c001a367dfa0
zhousg <zhoushugang@itcast.cn>
2022-12-29 17:20:13 +0800

    feat: 个人中心快捷工具和退出登录

--- src/views/User/index.vue
+++ src/views/User/index.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import { getUserInfo } from '@/services/user'
+import { useUserStore } from '@/stores'
 import type { UserInfo } from '@/types/user'
+import { showConfirmDialog } from 'vant'
 import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
 
 const user = ref<UserInfo>()
 
@@ -9,6 +12,29 @@ onMounted(async () => {
   const res = await getUserInfo()
   user.value = res.data
 })
+
+// 初始化快捷工具
+const tools = [
+  { label: '我的问诊', path: '/user/consult' },
+  { label: '我的处方', path: '/' },
+  { label: '家庭档案', path: '/user/patient' },
+  { label: '地址管理', path: '/' },
+  { label: '我的评价', path: '/' },
+  { label: '官方客服', path: '/' },
+  { label: '设置', path: '/' }
+]
+// 退出
+const store = useUserStore()
+const router = useRouter()
+const onLogout = async () => {
+  await showConfirmDialog({
+    title: '温馨提示',
+    message: '您是否确认退出优医问诊？'
+  })
+  // 点击的确认
+  store.delUser()
+  router.push('/login')
+}
 </script>
 
 <template>
@@ -74,6 +100,22 @@ onMounted(async () => {
         </van-col>
       </van-row>
     </div>
+    <!-- 快捷工具 -->
+    <div class="user-page-group">
+      <h3>快捷工具</h3>
+      <van-cell
+        v-for="(item, i) in tools"
+        :key="item.label"
+        :title="item.label"
+        :to="item.path"
+        is-link
+        :border="false"
+      >
+        <template #icon><cp-icon :name="`user-tool-0${i + 1}`" /></template>
+      </van-cell>
+    </div>
+    <!-- 退出登录 -->
+    <a href="javascript:;" class="logout" @click="onLogout">退出登录</a>
   </div>
 </template>
 
