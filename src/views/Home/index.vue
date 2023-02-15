be8504a8cbcfae203299bbbfb34229e498df86ca
zhousg <zhoushugang@itcast.cn>
2023-02-15 15:54:05 +0800

    feat: 首页模块-推荐关注组件

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import KnowledgeList from './components/KnowledgeList.vue'
+import FollowDoctor from './components/FollowDoctor.vue'
 
 const active = ref(1)
 </script>
@@ -82,6 +83,7 @@ const active = ref(1)
     <!-- TAB -->
     <van-tabs shrink sticky v-model:active="active">
       <van-tab title="关注">
+        <follow-doctor></follow-doctor>
         <knowledge-list type="like" />
       </van-tab>
       <van-tab title="推荐">
