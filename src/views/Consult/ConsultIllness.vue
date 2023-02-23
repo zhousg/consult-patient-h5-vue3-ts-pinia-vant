27f197d79cacc2a92485080ea91d4f6b37e5a7da
zhousg <zhoushugang@itcast.cn>
2023-02-23 16:02:57 +0800

    feat: 病情描述-上传组件

--- src/views/Consult/ConsultIllness.vue
+++ src/views/Consult/ConsultIllness.vue
@@ -22,6 +22,15 @@ const form = ref<ConsultIllness>({
   consultFlag: undefined,
   pictures: []
 })
+
+// 上传图片
+const fileList = ref([])
+const onAfterRead = () => {
+  console.log('选择图片，去上传')
+}
+const onDeleteImg = () => {
+  console.log('删除图片')
+}
 </script>
 
 <template>
@@ -56,6 +65,21 @@ const form = ref<ConsultIllness>({
         <p>此次病情是否去医院就诊过？</p>
         <cp-radio-btn :options="flagOptions" v-model="form.consultFlag" />
       </div>
+      <!-- 上传组件 -->
+      <div class="illness-img">
+        <van-uploader
+          upload-icon="photo-o"
+          upload-text="上传图片"
+          max-count="9"
+          :max-size="5 * 1024 * 1024"
+          v-model="fileList"
+          :after-read="onAfterRead"
+          @delete="onDeleteImg"
+        ></van-uploader>
+        <p class="tip" v-if="!fileList.length">
+          上传内容仅医生可见,最多9张图,最大5MB
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -64,6 +88,44 @@ const form = ref<ConsultIllness>({
 .consult-illness-page {
   padding-top: 46px;
 }
+.illness-img {
+  padding-top: 16px;
+  margin-bottom: 40px;
+  display: flex;
+  align-items: center;
+  .tip {
+    font-size: 12px;
+    color: var(--cp-tip);
+  }
+  ::v-deep() {
+    .van-uploader {
+      &__preview {
+        &-delete {
+          left: -6px;
+          top: -6px;
+          border-radius: 50%;
+          background-color: var(--cp-primary);
+          width: 20px;
+          height: 20px;
+          &-icon {
+            transform: scale(0.9) translate(-22%, 22%);
+          }
+        }
+        &-image {
+          border-radius: 8px;
+          overflow: hidden;
+        }
+      }
+      &__upload {
+        border-radius: 8px;
+      }
+      &__upload-icon {
+        color: var(--cp-text3);
+      }
+    }
+  }
+}
+
 .illness-tip {
   display: flex;
   padding: 15px;
