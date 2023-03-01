3fe3463cb0744f972cb37e0a6f4ff85b38031d36
zhousg <zhoushugang@itcast.cn>
2023-03-01 21:04:57 +0800

    feat: 问诊支付-用户引导

--- src/views/Consult/ConsultPay.vue
+++ src/views/Consult/ConsultPay.vue
@@ -2,10 +2,11 @@
 import { createConsultOrder, getConsultOrderPre } from '@/services/consult'
 import { getPatientDetail } from '@/services/user'
 import { useConsultStore } from '@/stores'
-import type { ConsultOrderPreData } from '@/types/consult'
+import type { ConsultOrderPreData, PartialConsult } from '@/types/consult'
 import type { Patient } from '@/types/user'
-import { showToast } from 'vant'
+import { showConfirmDialog, showDialog, showToast } from 'vant'
 import { ref, onMounted } from 'vue'
+import { onBeforeRouteLeave, useRouter } from 'vue-router'
 // 预支付信息
 const payInfo = ref<ConsultOrderPreData>()
 const store = useConsultStore()
@@ -26,7 +27,29 @@ const loadPatient = async () => {
   patient.value = res.data
 }
 
+type Key = keyof PartialConsult
 onMounted(() => {
+  // 生成订单需要的信息不完整的时候需要提示
+  const validKeys: Key[] = [
+    'type',
+    'illnessType',
+    'depId',
+    'illnessDesc',
+    'illnessTime',
+    'consultFlag',
+    'patientId'
+  ]
+  const valid = validKeys.every((key) => store.consult[key] !== undefined)
+  if (!valid) {
+    return showDialog({
+      title: '温馨提示',
+      message:
+        '问诊信息不完整请重新填写，如有未支付的问诊订单可在问诊记录中继续支付！',
+      closeOnPopstate: false
+    }).then(() => {
+      router.push('/')
+    })
+  }
   loadData()
   loadPatient()
 })
@@ -50,6 +73,32 @@ const submit = async () => {
   orderId.value = res.data.id
 
   show.value = true
+}
+
+// 提示1：取消支付将无法获得医生回复，医生接诊名额有限，是否确认关闭？
+// 提示2：问诊信息不完整请重新填写，如有未支付的问诊订单可在问诊记录中继续支付！
+
+// 用户引导
+onBeforeRouteLeave(() => {
+  if (orderId.value) return false
+})
+
+const router = useRouter()
+const onClose = () => {
+  return showConfirmDialog({
+    title: '温馨提示',
+    message: '取消支付将无法获得医生回复，医生接诊名额有限，是否确认关闭？',
+    cancelButtonText: '狠心离开',
+    confirmButtonText: '继续支付'
+  })
+    .then(() => {
+      return false
+    })
+    .catch(() => {
+      orderId.value = ''
+      router.push('/user/consult')
+      return true
+    })
 }
 </script>
 
@@ -95,7 +144,13 @@ const submit = async () => {
       :loading="loading"
     />
     <!-- 支付抽屉，控制面板 -->
-    <van-action-sheet v-model:show="show" title="选择支付方式">
+    <van-action-sheet
+      v-model:show="show"
+      title="选择支付方式"
+      :close-on-popstate="false"
+      :closeable="false"
+      :before-close="onClose"
+    >
       <div class="pay-type">
         <p class="amount">￥ {{ payInfo.actualPayment.toFixed(2) }}</p>
         <van-cell-group>
