a04ed4fa21709670887767f469568fd69ab6e361
zhousg <zhoushugang@itcast.cn>
2023-03-13 11:19:05 +0800

    feat: 评价医生-结束消息和评价消息

---
+++ src/views/Room/components/EvaluateCard.vue
@@ -0,0 +1,119 @@
+<script setup lang="ts">
+import type { EvaluateDoc } from '@/types/room'
+import { showToast } from 'vant'
+import { ref, computed } from 'vue'
+
+defineProps<{
+  evaluateDoc?: EvaluateDoc
+}>()
+
+// 收集数据
+const score = ref(0)
+const content = ref('')
+const anonymousFlag = ref(false)
+const disabled = computed(() => !score.value || !content.value)
+const onSubmit = () => {
+  if (!score.value) return showToast('请选择评分')
+  if (!content.value) return showToast('请填写评价')
+  // 提交评价信息
+}
+</script>
+
+<template>
+  <div class="evaluate-card" v-if="evaluateDoc">
+    <p class="title">医生服务评价</p>
+    <p class="desc">我们会更加努力提升服务质量</p>
+    <van-rate
+      :modelValue="3"
+      size="7vw"
+      gutter="3vw"
+      color="#FADB14"
+      void-icon="star"
+      void-color="rgba(0,0,0,0.04)"
+    />
+  </div>
+  <div class="evaluate-card" v-else>
+    <p class="title">感谢您的评价</p>
+    <p class="desc">本次在线问诊服务您还满意吗？</p>
+    <van-rate
+      size="7vw"
+      gutter="3vw"
+      color="#FADB14"
+      void-icon="star"
+      void-color="rgba(0,0,0,0.04)"
+      v-model="score"
+    />
+    <van-field
+      type="textarea"
+      maxlength="150"
+      show-word-limit
+      rows="3"
+      placeholder="请描述您对医生的评价或是在医生看诊过程中遇到的问题"
+      v-model="content"
+    ></van-field>
+    <div class="footer">
+      <van-checkbox v-model="anonymousFlag">匿名评价</van-checkbox>
+      <van-button
+        :class="{ disabled }"
+        @click="onSubmit"
+        type="primary"
+        size="small"
+        round
+      >
+        提交评价
+      </van-button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.evaluate-card {
+  width: 100%;
+  background-color: #fff;
+  border-radius: 8px;
+  overflow: hidden;
+  text-align: center;
+  padding: 15px;
+  .title {
+    font-size: 15px;
+    margin-bottom: 5px;
+  }
+  .desc {
+    font-size: 12px;
+    margin-bottom: 15px;
+    color: var(--cp-tip);
+  }
+  .van-field {
+    background-color: var(--cp-bg);
+    margin: 15px 0;
+    border-radius: 8px;
+  }
+  .footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    box-sizing: border-box;
+    :deep() {
+      .van-checkbox {
+        .van-icon {
+          font-size: 12px;
+        }
+        &__label {
+          font-size: 12px;
+          color: var(--cp-tip);
+        }
+        height: 16px;
+      }
+      .van-button {
+        padding: 0 16px;
+        &.disabled {
+          opacity: 1;
+          background: #fafafa;
+          color: #d9dbde;
+          border: #fafafa;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/Room/components/RoomMessage.vue
+++ src/views/Room/components/RoomMessage.vue
@@ -7,6 +7,7 @@ import type { Message } from '@/types/room'
 import { showImagePreview, showToast } from 'vant'
 import dayjs from 'dayjs'
 import { getPrescriptionPic } from '@/services/consult'
+import EvaluateCard from './EvaluateCard.vue'
 
 defineProps<{
   item: Message
@@ -76,11 +77,14 @@ const onShowPrescription = async (id?: string) => {
     </div>
   </div>
   <!-- 通知-结束 -->
-  <!-- <div class="msg msg-tip msg-tip-cancel">
+  <div
+    class="msg msg-tip msg-tip-cancel"
+    v-if="item.msgType === MsgType.NotifyCancel"
+  >
     <div class="content">
-      <span>订单取消</span>
+      <span>{{ item.msg.content }}</span>
     </div>
-  </div> -->
+  </div>
   <!-- 发送文字 -->
   <div
     class="msg msg-to"
@@ -160,6 +164,14 @@ const onShowPrescription = async (id?: string) => {
     </div>
   </div>
   <!-- 评价卡片，后期实现 -->
+  <div
+    class="msg msg-comment"
+    v-if="
+      item.msgType === MsgType.CardEva || item.msgType === MsgType.CardEvaForm
+    "
+  >
+    <evaluate-card :evaluateDoc="item.msg.evaluateDoc"></evaluate-card>
+  </div>
 </template>
 
 <style lang="scss" scoped>
